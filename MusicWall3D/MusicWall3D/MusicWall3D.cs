6cd7dd7fbb07d6d7cc6e3a8024b0c8a510651e0d
plundahl <petter.lundahl@gmail.com>
2013-10-24 13:36:37 +0200

    Can now play sound from sharpDX
    Will keep the code in this branch untill I've cleand it up, atm it looks
    a bit to ugly.
    Just wanted to show that I've accually been working.

--- MusicWall3D/MusicWall3D/MusicWall3D.cs
+++ MusicWall3D/MusicWall3D/MusicWall3D.cs
@@ -104,6 +104,7 @@ namespace MusicWall3D
         //TODO
         private Stopwatch stopWatch;
         private TimeSpan lastUpdate;
+        private Sound sound;
 
         public MusicWall3D()
         {
@@ -117,12 +118,15 @@ namespace MusicWall3D
             stopWatch.Start();
             lastUpdate = stopWatch.Elapsed;
 
+
         }
 
         public void Run()
         {
             form = new RenderForm("ComposIt");
             form.ClientSize = new Size(1380, 768);
+            //TODO
+            sound = new Sound(form.Handle);
 
             form.KeyDown += (target, arg) =>
             {
@@ -301,6 +305,7 @@ namespace MusicWall3D
             {
                 objects.Clear();
                 splines.Clear();
+                sound.Clear();
 
                 currentPoints = new List<Vector2>();
             }
@@ -360,6 +365,7 @@ namespace MusicWall3D
                 {
                     objects.Add(currentPoints);
                     splines.Add(currentSpline);
+                    sound.addCurve(currentPoints);
                 }
             }
 
@@ -415,7 +421,7 @@ namespace MusicWall3D
             }
             if (lastUpdate.Seconds != tmp.Seconds)
             {
-                Sound sound = new Sound();
+                
                 lastUpdate = tmp;
                 float last, first;
                 foreach (List<Vector2> list in objects)
@@ -426,7 +432,7 @@ namespace MusicWall3D
                     last = (list.Last()[0]) + 0.05f;
                     if ((first * 10) < tmp.Seconds % 10 && (int)(last * 10) >= tmp.Seconds % 10)
                     {
-                        sound.Play(list[0].Y);                      
+//                        sound.Play(list[0].Y);                      
                     }
                     //Debug.WriteLine(first * 10 + "<" + tmp.Seconds % 10 + "=" + (first * 10 < tmp.Seconds % 10));
                     //Debug.WriteLine(last * 10 + ">" + tmp.Seconds % 10 + "=" + (last * 10 > tmp.Seconds % 10));
@@ -434,7 +440,7 @@ namespace MusicWall3D
 
                 }
             }
-        }
+        } 
 
         private void addParticles()
         {
@@ -452,9 +458,6 @@ namespace MusicWall3D
                         float x = list[0].X;
                         float y = list[0].Y;
                     }
-                    //Debug.WriteLine(first * 10 + "<" + tmp.Seconds % 10 + "=" + (first * 10 < tmp.Seconds % 10));
-                    //Debug.WriteLine(last * 10 + ">" + tmp.Seconds % 10 + "=" + (last * 10 > tmp.Seconds % 10));
-                    //Debug.WriteLine(0.5f);
                 }
             }
         }
--- MusicWall3D/MusicWall3D/Sound.cs
+++ MusicWall3D/MusicWall3D/Sound.cs
@@ -1,22 +1,75 @@
 ﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Bespoke.Common;
+//using System.Threading;
+//using System.Net;
+using SharpDX;
+using SharpDX.Multimedia;
+using SharpDX.DirectSound;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Bespoke.Common;
-using Bespoke.Common.Osc;
-using System.Threading;
-using System.Net;
+
+using System.Diagnostics;
+
+
+
 
 namespace MusicWall3D
 {
 
         public class Sound
         {
-            private static readonly IPEndPoint Destination = new IPEndPoint(0X0100007f, 57120);
-            private Thread mTransmitterThread;
-            private OscPacket mPacket;
+            private SecondarySoundBuffer secondarySoundBuffer;
+            private BufferCapabilities capabilities;
+            private int soundLength = 10;
+            WaveFormat waveFormat;
+            short[] sounds;
+            int lastFreq = -1;
+            short lastVal = -1;
+            int offset = 0;
+       
+            public Sound(IntPtr handle)
+            {
+                DirectSound directSound = new DirectSound();
+                // Set Cooperative Level to PRIORITY (priority level can call the SetFormat and Compact methods)
+                //
+                directSound.SetCooperativeLevel(handle, CooperativeLevel.Priority);
 
+                // Create PrimarySoundBuffer
+                var primaryBufferDesc = new SoundBufferDescription();
+                primaryBufferDesc.Flags = BufferFlags.PrimaryBuffer;
+                primaryBufferDesc.AlgorithmFor3D = Guid.Empty;
+
+                var primarySoundBuffer = new PrimarySoundBuffer(directSound, primaryBufferDesc);
+
+                // Play the PrimarySound Buffer
+                primarySoundBuffer.Play(0, PlayFlags.Looping);
+
+                // Default WaveFormat Stereo 44100 16 bit
+                waveFormat = new WaveFormat();
+
+                // Create SecondarySoundBuffer
+                var secondaryBufferDesc = new SoundBufferDescription();
+                secondaryBufferDesc.BufferBytes = waveFormat.ConvertLatencyToByteSize(10000);
+                secondaryBufferDesc.Format = waveFormat;
+                secondaryBufferDesc.Flags = BufferFlags.GetCurrentPosition2 | BufferFlags.ControlPositionNotify | BufferFlags.GlobalFocus |
+                                            BufferFlags.ControlVolume | BufferFlags.StickyFocus;
+                secondaryBufferDesc.AlgorithmFor3D = Guid.Empty;
+                secondarySoundBuffer = new SecondarySoundBuffer(directSound, secondaryBufferDesc);
+
+                // Get Capabilties from secondary sound buffer
+                capabilities = secondarySoundBuffer.Capabilities;
+                sounds = new short[capabilities.BufferBytes / waveFormat.BlockAlign];
+
+                // Play the song
+                secondarySoundBuffer.Play(0, PlayFlags.Looping);
+
+                for(int i = 0; i < sounds.Length; i++){
+                    sounds[i] = 0;
+                }
+            }
             //Takes a mouseState.Y (float between 0 and 1) and playes a tune.
             public void Play(float Y)
             {
@@ -25,58 +78,134 @@ namespace MusicWall3D
                     Kick();
                 else if (Y > 80)
                     Snare();
-                else
-                    Note(10 * (100 - (int)Y));
+                //else
+                    //Note(10 * (100 - (int)Y));
             }
 
+            public void addCurve(List<Vector2> points)
+            {
+                int i = 0;
+                int freq = -1;
+                lastFreq = -1;
+                lastVal = -1;                
+                int numberOfSamples = capabilities.BufferBytes / waveFormat.BlockAlign;
+
+                Debug.WriteLine(points.Count);
+                if (points.Count == 1)
+                {
+                    Note(points[0]);
+                }
+                else
+                {
+                    offset = (int)(points[0].X * numberOfSamples);
+                    foreach (Vector2 p in points)
+                    {
+                        if (p.X <= 1)
+                        {
+                            while (i < p.X * numberOfSamples)
+                            {
+                                if (freq != -1)
+                                {
+                                    //short value = (short)(Math.Sin(2 * Math.PI * (freq) * i / waveFormat.SampleRate) * 1000); // Not too loud
+                                    short value = smoothCurve(freq, i);
+                                    sounds[i] = (short)((int)sounds[i] + (int)value - (int)(sounds[i] * value) / short.MaxValue);
+                                }
+                                i++;
+                            }
+                            freq = (int)(800 - p.Y * 800);
+                        }
+                    }
+                    // Lock the buffer
+                    DataStream dataPart2;
+                    var dataPart1 = secondarySoundBuffer.Lock(0, capabilities.BufferBytes, LockFlags.EntireBuffer, out dataPart2);
+                    foreach (short value in sounds)
+                    {
+                        dataPart1.Write(value);
+                        dataPart1.Write(value);
+                    }
+                    // Unlock the buffer
+                    secondarySoundBuffer.Unlock(dataPart1, dataPart2);
+                }
+            }
+
+            public void Clear() {
+                sounds = new short[sounds.Length];
+                // Lock the buffer
+                DataStream dataPart2;
+                var dataPart1 = secondarySoundBuffer.Lock(0, capabilities.BufferBytes, LockFlags.EntireBuffer, out dataPart2);
+                foreach (short value in sounds)
+                {
+                    dataPart1.Write(value);
+                    dataPart1.Write(value);
+                }
+                // Unlock the buffer
+                secondarySoundBuffer.Unlock(dataPart1, dataPart2);
+
+            }
+
+            private short smoothCurve(int freq, int i) {
+                if (lastFreq == -1)
+                    lastFreq = freq;
+                short value = (short)(Math.Sin(2 * Math.PI * (lastFreq) * (i-offset) / waveFormat.SampleRate) * 1000); // Not too loud
+                if (freq != lastFreq)
+                {
+                    if (lastVal < 0 && value >= 0)
+                    {
+                        lastFreq = freq;
+                        offset = i;
+                        value = (short)(Math.Sin(2 * Math.PI * (lastFreq) * (i - offset) / waveFormat.SampleRate) * 1000); // Not too loud
+                    }
+                }
+                lastVal = value;
+                return value;
+            }
 
             private void Kick()
             {
-                OscMessage message = new OscMessage(Destination, "/soundWall/kick");
-                //message.Append(333);
-                sendOsc(message);
             }
             private void Snare()
             {
-                OscMessage message = new OscMessage(Destination, "/soundWall/snare");
-                sendOsc(message);
             }
-            private void Note(int freq)
+            private void Note(Vector2 point)
             {
-                OscMessage message = new OscMessage(Destination, "/soundWall/note");
-                message.Append(freq);
-                sendOsc(message);
+                int freq = (int)(point.Y * 800);
+                lastFreq = -1;
+                lastVal = -1;
+                int numberOfSamples = capabilities.BufferBytes / waveFormat.BlockAlign;
+                int i = (int)(point.X * numberOfSamples);
+                offset = i;
+                for (int j = 0; j < 10000; j++) {
+                    short value = (short)((smoothCurve(freq, i)*2.0f*(float)(10000-j)/10000.0f));
+                    sounds[i] = (short)((int)sounds[i] + (int)value - (int)(sounds[i] * value) / short.MaxValue);
+                    i++;
+                    if (i == sounds.Length)
+                        break;
+                }
+                // Lock the buffer
+                DataStream dataPart2;
+                var dataPart1 = secondarySoundBuffer.Lock(0, capabilities.BufferBytes, LockFlags.EntireBuffer, out dataPart2);
+                foreach (short value in sounds)
+                {
+                    dataPart1.Write(value);
+                    dataPart1.Write(value);
+                }
+                // Unlock the buffer
+                secondarySoundBuffer.Unlock(dataPart1, dataPart2);
+
+
             }
 
-            private void sendOsc(OscMessage message)
+            private void sendOsc()
             {
-                OscBundle bundle = new OscBundle(Destination); 
-                bundle.Append(message);
-                OscPacket packet = bundle;
-
-                Assert.ParamIsNotNull(bundle);
-                mPacket = packet;
-                mTransmitterThread = new Thread(RunWorker);
-                mTransmitterThread.Start();
 
             }
 
             public void Stop()
             {
-                mTransmitterThread.Join();
             }
 
             private void RunWorker()
             {
-                try
-                {
-                    mPacket.Send(Destination);
-                    Thread.Sleep(1000);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
             }
 
         }
